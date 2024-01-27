6461b6a53be665fd9d72af4eb98289202166170f
Al-Shahriar-Nazim <nazimking7634@gmail.com>
2024-01-27 16:08:41 +0600

    added this js file

---
+++ two.js
@@ -0,0 +1,29 @@
+// console.log(5>10);
+// console.log(5<10);
+// console.log(10==10);
+// console.log(5==10);
+// console.log(10>=10);
+// console.log( 15>=10);
+// console.log(5>=10);
+// console.log(5<=10);
+// console.log(10<=10);
+// console.log(100<=10);
+// console.log(10==10);
+
+// --------------------variable declearation for coding -------------------
+// var peyaraPrice=40;
+// var applePrice =250;
+// console.log(peyaraPrice>applePrice);
+// console.log(peyaraPrice<applePrice);
+
+/**
+ * double equal ==
+ * and
+ * thriple equal  ===
+ */
+console.log(10==10);
+console.log(10 == '10');
+console.log(10===10);
+console.log(10 ==='10');
+console.log(10 !=10);
+console.log(10 !==10);
